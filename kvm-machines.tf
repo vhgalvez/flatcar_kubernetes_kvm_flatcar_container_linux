ff9dabc60ef3afa8195c73a143afc6714d947c17
Victor Galvez <vhgalvez@gmail.com>
2024-03-02 00:52:29 +0100

    Update kvm-machines.tf

--- kvm-machines.tf
+++ kvm-machines.tf
@@ -1,18 +1,20 @@
 terraform {
-  required_version = ">= 0.13"
+  required_version = ">= 0.13.0"
   required_providers {
     libvirt = {
       source  = "dmacvicar/libvirt"
+      version = "~> 0.7.6"
     }
     ct = {
       source  = "poseidon/ct"
+      version = "~> 0.13.0"
     }
     template = {
       source  = "hashicorp/template"
+      version = "~> 2.2.0"
     }
   }
 }
-
 # Define libvirt URI
 provider "libvirt" {
   uri = "qemu:///system"
@@ -30,7 +32,7 @@ resource "libvirt_volume" "vm-disk" {
   for_each = toset(var.machines)
 
   # workaround: depend on libvirt_ignition.ignition[each.key], otherwise the VM will use the old disk when the user-data changes
-  
+
   name           = "${var.cluster_name}-${each.key}-${md5(libvirt_ignition.ignition[each.key].id)}.qcow2"
   base_volume_id = libvirt_volume.base.id
   pool           = var.storage_pool
@@ -61,7 +63,7 @@ resource "libvirt_domain" "machine" {
     listen_type = "address"
   }
 
-# dynamic IP assignment on the bridge, NAT for Internet access
+  # dynamic IP assignment on the bridge, NAT for Internet access
   network_interface {
     network_name   = var.network_name
     wait_for_lease = true
@@ -78,8 +80,8 @@ data "template_file" "vm-configs" {
   template = file("${path.module}/machine-${each.key}.yaml.tmpl")
 
   vars = {
-    ssh_keys = jsonencode(var.ssh_keys)
-    name     = each.key
+    ssh_keys  = jsonencode(var.ssh_keys)
+    name      = each.key
     host_name = "${each.key}.${var.cluster_name}.${var.cluster_domain}"
   }
 }
