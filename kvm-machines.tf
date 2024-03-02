9c954577e8625710c7c6f18f9dabc1109fa7e99d
Victor Galvez <vhgalvez@gmail.com>
2024-03-02 01:04:53 +0100

    Update kvm-machines.tf

--- kvm-machines.tf
+++ kvm-machines.tf
@@ -7,7 +7,7 @@ terraform {
     }
     ct = {
       source  = "poseidon/ct"
-      version = "~> 0.14.0"
+      version = "~> 0.13.0"
     }
      template = {
       source  = "hashicorp/template"
